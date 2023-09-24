d6737b09c51f3e9f7fe5cdd8c40463e480d143d0
DISTORTION <3347432324@QQ.com>
2023-09-24 19:27:13 +0800

    9.24 homework FINAL VERSION
    chapter two homework

--- programs/2_1_2.c
+++ programs/2_1_2.c
@@ -1,29 +1,46 @@
 #include<stdio.h>
 
 int all_days(int month);
+int month_days[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+
 int main(void){
-    int input_month , days , which_day , j = 1;
+    int input_month , days , which_day , month_day , j = 1 , i , k = 0;
     printf("please input month:");
     scanf("%d" , &input_month);
     days = all_days(input_month);
-    which_day = (days - 1)%7;//2023年1月1号是星期日
+    month_day = month_days[input_month];
+    which_day = (days)%7;//2023年1月1号是星期日
 
     printf("SUN  ");printf("MON  ");printf("TUE  ");printf("WED  ");printf("THU  ");printf("FRI  ");printf("SAT");
     printf("\n");
 
-    while(days != 0){
-        switch (which_day){
-        case 0:
-        printf("%3d" , j);
-        j++;
-        days -= days;
- 
-        default:
-        printf("   ");
-        which_day--;
+    i = which_day;
+    while(month_day != 0){
+
+        if(i == 0){
+            k = which_day + k + 1;
+            which_day = 0;
+            
+            if(k == 7){
+            printf("%3d\n" , j);
+            j++;
+            month_day--;
+            k = 0;
+            }
+            else{
+            printf("%3d" , j);   
+            printf("  ");
+            j++;
+            month_day--;
+            }
+
         }
-        
-        printf("  ");
+        else{
+        printf("     ");
+        i--;
+        }
+
+
     }
 
     _getch();
@@ -31,12 +48,12 @@ int main(void){
 }
 
 int all_days(int month){
-    int month_days[13] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
     int all_days = 0;
+    int i = 0;
 
-    for(int i = 0 ; i <= month ; i++){
+    for( ; i < month ; i++){
         all_days = all_days + month_days[i];
     }
-
+    
     return all_days;
 }
